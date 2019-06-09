c34dbe72415d8b5fce1704c7f750f99e89473ae8
Enrique Mendoza <enrique.510.06@gmail.com>
2019-06-08 21:20:39 -0600

    Se creo la pantalla para editar perfil

--- controllers/perfilController.py
+++ controllers/perfilController.py
@@ -4,3 +4,7 @@ from flask import session,render_template,redirect,url_for, request
 @app.route("/perfil",methods = ['POST', 'GET'])
 def perfil():
      return render_template('perfil/perfil.html')
+     
+@app.route("/editarPerfil",methods = ['POST', 'GET'])
+def editarPerfil():
+     return render_template('perfil/editarperfil.html')
